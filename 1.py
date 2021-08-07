2bc888ca230e04538c1534d240348fa2ffd46649
Sayda U. Hamida <85420828+suhamida@users.noreply.github.com>
2021-08-07 16:44:13 +0600

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,7 @@
+#virtual Friend
+#https://pypi.org/project/pyttsx3/#description  is the address to find the code.
+
+import pyttsx3
+friend = pyttsx3.init()
+friend.say("Congratualions! Finally you make this happened. ")
+friend.runAndWait()
